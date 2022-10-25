86a9e8e6536e817413a89b6e82b06aaace822f87
kaligurl2008 <kaligurl2008@gmail.com>
2022-10-25 15:23:48 -0400

    got rid of second span, cleaned up the css, and finished the javascript

--- java.js
+++ java.js
@@ -1,11 +1,46 @@
 //alert('connected');
+//const quote = document.getElementById("quote");
 
 //set up event listener for botton
 document.getElementById("quote-btn").addEventListener('click', getQuote)
 
+//set up variables
+const quote = document.getElementById("quote");
+
+const author = document.getElementById("author");
+
+//const url = "https://api.quotable.io/random";
+
+/*fetch (url)
+.then(response => response.json())
+.then(data =>{
+    console.log(data);
+    console.log(data.content);
+    console.log(data.author);
+})
+*/
+
 function getQuote (){
-    console.log("clicked");
+    //console.log("clicked");
+    const url = "https://api.quotable.io/random";
+    //fetch data
+    fetch (url)
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data);
+        console.log(data.content);
+        quote.innerHTML = `
+            <h3>${data.content}</h3>
+            <h1>${data.author}</h1>`;
+
+        console.log(data.author);
+    })
+
+
+
 }
 
-const quote = document.getElementById("quote");
+
+
+
 
