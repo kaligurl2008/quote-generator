18fb84d110c96cee4dbdabe6348cf4984c10e5ae
kaligurl2008 <kaligurl2008@gmail.com>
2022-10-25 10:53:03 -0400

    added event listener

--- java.js
+++ java.js
@@ -1,1 +1,11 @@
 //alert('connected');
+
+//set up event listener for botton
+document.getElementById("quote-btn").addEventListener('click', getQuote)
+
+function getQuote (){
+    console.log("clicked");
+}
+
+const quote = document.getElementById("quote");
+
